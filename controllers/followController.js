93f82b5257ce1f381f66c47f9ae5ca487affd09d
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-08-21 21:55:06 +0300

    Update followController.js

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,28 +1,27 @@
 const Follow = require('../models/Follow')
-exports.addFollow = function(req,res){
-    let follow = new Follow(req.params.username,req.visitorId)
-    follow.create().then(()=>{
-        req.flash("success",`Successfully followed ${req.params.username}`)
-        req.session.save(()=> res.redirect(`/profile/${req.params.username}`))
-    }).catch((errors)=>{
-        errors.forEach(error=>{
-            req.flash("errors",error)
-        })
-        req.session.save(()=>res.redirect('/'))
 
+exports.addFollow = function(req, res) {
+  let follow = new Follow(req.params.username, req.visitorId)
+  follow.create().then(() => {
+    req.flash("success", `Successfully followed ${req.params.username}`)
+    req.session.save(() => res.redirect(`/profile/${req.params.username}`))
+  }).catch((errors) => {
+    errors.forEach(error => {
+      req.flash("errors", error)
     })
-    exports.removeFollow = function(req,res){
-        let follow = new Follow(req.params.username,req.visitorId)
-        follow.delete().then(()=>{
-            req.flash("success",`Successfully stopped ${req.params.username}`)
-            req.session.save(()=> res.redirect(`/profile/${req.params.username}`))
-        }).catch((errors)=>{
-            errors.forEach(error=>{
-                req.flash("errors",error)
-            })
-            req.session.save(()=>res.redirect('/'))
-    
-        })
-    }
+    req.session.save(() => res.redirect('/'))
+  })
+}
 
+exports.removeFollow = function(req, res) {
+  let follow = new Follow(req.params.username, req.visitorId)
+  follow.delete().then(() => {
+    req.flash("success", `Successfully unfollowed ${req.params.username}`)
+    req.session.save(() => res.redirect(`/profile/${req.params.username}`))
+  }).catch((errors) => {
+    errors.forEach(error => {
+      req.flash("errors", error)
+    })
+    req.session.save(() => res.redirect('/'))
+  })
 }
