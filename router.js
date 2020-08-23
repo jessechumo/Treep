402a3ff34e945c6806cc6fa71fde0801bd6cf648
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-08-23 15:07:18 +0300

    Update router.js

--- router.js
+++ router.js
@@ -13,6 +13,7 @@ router.post('/logout', userController.logout)
 // profile related routes
 router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen)
 router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen)
+router.get('/profile/:username/following', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowingScreen)
 
 // post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
