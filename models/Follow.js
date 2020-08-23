f610f543d1e3a9c2546a7fb8ac187e9b5dc1aa8a
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-08-23 15:04:54 +0300

    Update Follow.js

--- models/Follow.js
+++ models/Follow.js
@@ -1,92 +1,116 @@
 const usersCollection = require('../db').db().collection("users")
 const followsCollection = require('../db').db().collection("follows")
-//const { promiseImpl } = require("ejs")
-const { ObjectID } = require('mongodb')
+const ObjectID = require('mongodb').ObjectID
+const User = require('./User')
 
-let Follow = function(followedUsername, authorId){
-this.followedUsername = followedUsername
-this.authorId = authorId
-this.errors = []
+let Follow = function(followedUsername, authorId) {
+  this.followedUsername = followedUsername
+  this.authorId = authorId
+  this.errors = []
 }
 
-Follow.prototype.cleanUp=  async function(){
-    if(typeof(this.followedUsername) != "string"){
-        this.followedUsername=""
-    }
+Follow.prototype.cleanUp = async function() {
+  if (typeof(this.followedUsername) != "string") {this.followedUsername = ""}
 }
 
-Follow.prototype.validate = async  function(action){
-    //followedUsername must exist in database
-    let followedAccount =  await usersCollection.findOne({username: this.followedUsername})
-    if (followedAccount){
-        this.followedId = followedAccount._id
-    }
-    else{
-        this.errors.push("you cannot follow a user that does not exist.")
-    }
-    let doesFollowAlreadyExist = await followsCollection.findOne({followedId:this.followedId, authorId: new ObjectID(this.authorId)})
-    if (action == "create"){
-        if(doesFollowAlreadyExist){this.errors.push("you are already following this user")}
-    }
-    if (action == "delete"){
-        if(!doesFollowAlreadyExist){this.errors.push("you cannot unfollow someone you don't follow ")}
-    }
-    // you should not follow yourself
-    if(this.followedId.equals(this.authorId)) {this.errors.push("You cannot follow yourself!")}
+Follow.prototype.validate = async function(action) {
+  // followedUsername must exist in database
+  let followedAccount = await usersCollection.findOne({username: this.followedUsername})
+  if (followedAccount) {
+    this.followedId = followedAccount._id
+  } else {
+    this.errors.push("You cannot follow a user that does not exist.")
+  }
+
+  let doesFollowAlreadyExist = await followsCollection.findOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+  if (action == "create") {
+    if (doesFollowAlreadyExist) {this.errors.push("You are already following this user.")}
+  }
+  if (action == "delete") {
+    if (!doesFollowAlreadyExist) {this.errors.push("You cannot stop following someone you do not already follow.")}
+  }
+
+  // should not be able to follow yourself
+  if (this.followedId.equals(this.authorId)) {this.errors.push("You cannot follow yourself.")}
+
 }
 
-Follow.prototype.create= function(){
-    return new Promise(async(resolve,reject)=>{
-        this.cleanUp()
-        await this.validate("create")
-        if(!this.errors.length){
-            await followsCollection.insertOne({followedId: this.followedId,authorId:new ObjectID(this.authorId)})
-            resolve()
-        }
-        else{
-             reject(this.errors)
-        }
-    })
-}
-
-Follow.prototype.delete= function(){
-    return new Promise(async(resolve,reject)=>{
-        this.cleanUp()
-        await this.validate("delete")
-        if(!this.errors.length){
-            await followsCollection.deleteOne({followedId: this.followedId,authorId:new ObjectID(this.authorId)})
-            resolve()
-        }
-        else{
-             reject(this.errors)
-        }
-    })
-}
-
-
-Follow.isVisitorFollowing = async function(followedId,visitorId){
-    let followDoc = await followsCollection.findOne({followedId:followedId, authorId:new ObjectID(visitorId)})
-    if(followDoc){
-        return true
+Follow.prototype.create = function() {
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp()
+    await this.validate("create")
+    if (!this.errors.length) {
+      await followsCollection.insertOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+      resolve()
+    } else {
+      reject(this.errors)
     }
-    else{
-        return false
-    }
+  })
 }
-Follow.getFollowersById = function(id){
-    return new Promise(async(resolve,reject)=>{
-       try{
+
+Follow.prototype.delete = function() {
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp()
+    await this.validate("delete")
+    if (!this.errors.length) {
+      await followsCollection.deleteOne({followedId: this.followedId, authorId: new ObjectID(this.authorId)})
+      resolve()
+    } else {
+      reject(this.errors)
+    }
+  })
+}
+
+Follow.isVisitorFollowing = async function(followedId, visitorId) {
+  let followDoc = await followsCollection.findOne({followedId: followedId, authorId: new ObjectID(visitorId)})
+  if (followDoc) {
+    return true
+  } else {
+    return false
+  }
+}
+
+Follow.getFollowersById = function(id) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let followers = await followsCollection.aggregate([
+        {$match: {followedId: id}},
+        {$lookup: {from: "users", localField: "authorId", foreignField: "_id", as: "userDoc"}},
+        {$project: {
+          username: {$arrayElemAt: ["$userDoc.username", 0]},
+          email: {$arrayElemAt: ["$userDoc.email", 0]}
+        }}
+      ]).toArray()
+      followers = followers.map(function(follower) {
+        let user = new User(follower, true)
+        return {username: follower.username, avatar: user.avatar}
+      })
+      resolve(followers)
+    } catch {
+      reject()
+    }
+  })
+}
+Follow.getFollowingById = function(id) {
+    return new Promise(async (resolve, reject) => {
+      try {
         let followers = await followsCollection.aggregate([
-            {$match: {followedId}},
-            {$lookup: {from:"users", localField:"authorId" ,foreignField:"_id", as:"userDoc"}},
-            {$project:{
-                username:{ $arrayELemAt:["$userDoc.username", 0]},
-                email:{ $arrayELemAt:["$userDoc.email", 0]}
-            }}
-       }catch{
-           reject()
-       }
+          {$match: {authorId: id}},
+          {$lookup: {from: "users", localField: "followedId", foreignField: "_id", as: "userDoc"}},
+          {$project: {
+            username: {$arrayElemAt: ["$userDoc.username", 0]},
+            email: {$arrayElemAt: ["$userDoc.email", 0]}
+          }}
+        ]).toArray()
+        followers = followers.map(function(follower) {
+          let user = new User(follower, true)
+          return {username: follower.username, avatar: user.avatar}
+        })
+        resolve(followers)
+      } catch {
+        reject()
+      }
     })
-}
+  }
 
 module.exports = Follow
