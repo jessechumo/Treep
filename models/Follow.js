40a7c0b6db0360ff939bf8890c32f7e5e021617e
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-08-27 21:55:47 +0300

    Update Follow.js

--- models/Follow.js
+++ models/Follow.js
@@ -91,39 +91,41 @@ Follow.getFollowersById = function(id) {
     }
   })
 }
-Follow.getFollowingById = function(id) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let followers = await followsCollection.aggregate([
-          {$match: {authorId: id}},
-          {$lookup: {from: "users", localField: "followedId", foreignField: "_id", as: "userDoc"}},
-          {$project: {
-            username: {$arrayElemAt: ["$userDoc.username", 0]},
-            email: {$arrayElemAt: ["$userDoc.email", 0]}
-          }}
-        ]).toArray()
-        followers = followers.map(function(follower) {
-          let user = new User(follower, true)
-          return {username: follower.username, avatar: user.avatar}
-        })
-        resolve(followers)
-      } catch {
-        reject()
-      }
-    })
-  }
-  Follow.countFollowersById = function(id) {
-    return new Promise(async(resolve, reject)=>{
-      let followerCount=  await followsCollection.countDocuments({followedId: id})
-      resolve(followerCount)
-    } )
-  }
 
-  Follow.countFollowersById = function(id) {
-    return new Promise(async(resolve, reject)=>{
-      let count=  await followsCollection.countDocuments({authorId: id})
-      resolve(count)
-    } )
-  }
+Follow.getFollowingById = function(id) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let followers = await followsCollection.aggregate([
+        {$match: {authorId: id}},
+        {$lookup: {from: "users", localField: "followedId", foreignField: "_id", as: "userDoc"}},
+        {$project: {
+          username: {$arrayElemAt: ["$userDoc.username", 0]},
+          email: {$arrayElemAt: ["$userDoc.email", 0]}
+        }}
+      ]).toArray()
+      followers = followers.map(function(follower) {
+        let user = new User(follower, true)
+        return {username: follower.username, avatar: user.avatar}
+      })
+      resolve(followers)
+    } catch {
+      reject()
+    }
+  })
+}
+
+Follow.countFollowersById = function(id) {
+  return new Promise(async (resolve, reject) => {
+    let followerCount = await followsCollection.countDocuments({followedId: id})
+    resolve(followerCount)
+  })
+}
+
+Follow.countFollowingById = function(id) {
+  return new Promise(async (resolve, reject) => {
+    let count = await followsCollection.countDocuments({authorId: id})
+    resolve(count)
+  })
+}
 
 module.exports = Follow
