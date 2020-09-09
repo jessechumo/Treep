3c458bf0b3af18c16bb2ac33c178dd1c8add3d60
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-09-09 23:38:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,49 +7,54 @@ const app = express()
 const sanitizeHTML = require('sanitize-html')
 
 let sessionOptions = session({
-
-    secret:"node rocks!",
-    store:new MongoStore({client:require('./db')}),
-    resave : false,
-    saveUninitialized: false,
-    cookie:{maxAge:1000*60*60*24,httpOnly:true}
+  secret: "JavaScript is sooooooooo coool",
+  store: new MongoStore({client: require('./db')}),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
 })
 
 app.use(sessionOptions)
 app.use(flash())
 
-app.use(function(req,res,next) {
-   //make our markdown function available from within ejs templates
-   res.locals.filterUserHTML = function(content){
-    return sanitizeHTML(markdown(content),{allowedTags:['p','br','ul','dl','ol','bold','strong','li','em','i','h1','h2','h3','h4','h5','h6'],allowedAttributes:{}})
-   }
+app.use(function(req, res, next) {
+  // make our markdown function available from within ejs templates
+  res.locals.filterUserHTML = function(content) {
+    return sanitizeHTML(markdown(content), {allowedTags: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'bold', 'i', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'], allowedAttributes: {}})
+  }
+  
+  // make all error and success flash messages available from all templates
+  res.locals.errors = req.flash("errors")
+  res.locals.success = req.flash("success")
 
-
-    //make all error and success messages available from all templates
-    res.locals.errors = req.flash("errors") 
-    res.locals.success = req.flash("success") 
-
-
-    // make current user id  available on the req object
-    if(req.session .user){req.visitorId = req.session.user._id} else{req.visitorId=0}
-
-
-
-    //make user session data available from within view templates
-    res.locals.user =req.session.user
-    next()
+  // make current user id available on the req object
+  if (req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
+  
+  // make user session data available from within view templates
+  res.locals.user = req.session.user
+  next()
 })
 
 const router = require('./router')
-const { urlencoded } = require('express')
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
-app.use('/',router)
+app.use('/', router)
 
-module.exports=app
+
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server)
+
+io.on('connection', function(socket){
+  socket.on('chatMessageFromBrowser',function(data){
+    io.emit('chatMessageFromServer',{message:data.message})
+  })
+})
+
+module.exports = server
