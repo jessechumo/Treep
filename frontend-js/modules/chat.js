16d074e7bd7ca4dd9575a567312f5b8b46bb430e
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-09-10 23:15:29 +0300

    Update chat.js

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -1,9 +1,12 @@
+import DOMPurify from  'dompurify'
+
 export default class Chat {
     constructor() {
     this.openedYet = false
     this.chatWrapper = document.querySelector("#chat-wrapper")
     this.openIcon = document.querySelector(".header-chat-icon")
     this.injectHTML()
+    this.chatLog = document.querySelector("#chat")
     this.chatField = document.querySelector("#chatField")
     this.chatForm = document.querySelector("#chatForm")
     this.closeIcon = document.querySelector(".chat-title-bar-close")
@@ -21,6 +24,19 @@ export default class Chat {
     // Methods
     sendMessageToServer() {
     this.socket.emit('chatMessageFromBrowser', {message: this.chatField.value})
+    this.chatLog.insertAdjacentHTML('beforeend',DOMPurify.sanitize(`
+    <!-- template for your own message -->
+    <div class="chat-self">
+        <div class="chat-message">
+        <div class="chat-message-inner">
+        ${this.chatField.value}
+        </div>
+    </div>
+        <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+    </div>
+    <!-- end template-->
+    `))
+    this.chatLog.scrollTop = this.chatLog.scrollHeight
     this.chatField.value = ''
     this.chatField.focus()
     }
@@ -33,13 +49,32 @@ export default class Chat {
     }
     this.openedYet = true
     this.chatWrapper.classList.add("chat--visible")
+    this.chatField.focus()
     }
     openConnection() {
     this.socket = io()
-    this.socket.on('chatMessageFromServer', function(data) {
-        alert(data.message)
+    this.socket.on("welcome",(data)=>{
+        this.username = data.username
+        this.avatar = data.avatar
+    })
+    this.socket.on('chatMessageFromServer', (data)=> {
+        this.displayMessageFromServer(data)
     })
     }
+    displayMessageFromServer(data){
+        this.chatLog.insertAdjacentHTML("beforeend",DOMPurify.sanitize(`
+        <!-- template for messages from others -->
+    <div class="chat-other">
+        <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
+        <div class="chat-message"><div class="chat-message-inner">
+        <a href="/profile/${data.username}"><strong>${data.username}:</strong></a>
+        ${data.message}
+        </div></div>
+    </div>
+    <!-- end template-->`))
+    this.chatLog.scrollTop = this.chatLog.scrollHeight
+    }
+
     injectHTML() {
     this.chatWrapper.innerHTML = `
     <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
