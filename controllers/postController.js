53ee7409c2b625241e895bbff4fc2deeca273f84
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-09-10 23:18:11 +0300

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -18,7 +18,7 @@ exports.create = function(req, res) {
 exports.viewSingle = async function(req, res) {
   try {
     let post = await Post.findSingleById(req.params.id,req.visitorId)
-    res.render('single-post-screen', {post: post})
+    res.render('single-post-screen', {post: post , title:post.title})
   } catch {
     res.render('404')
   }
