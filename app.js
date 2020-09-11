36e2f4aa015ef6458603461a4ba034bcbdc6baee
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-09-11 20:12:52 +0300

    final update

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
 const markdown = require('marked')
+const csrf = require('csurf')
 const app = express()
 const sanitizeHTML = require('sanitize-html')
+
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use('/api',require('./router-api'))
 
 let sessionOptions = session({
   secret: "JavaScript is sooooooooo coool",
@@ -37,32 +44,46 @@ app.use(function(req, res, next) {
 
 const router = require('./router')
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
 
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
+app.use(csrf())
+
+app.use(function(req, res, next) {
+  res.locals.csrfToken = req.csrfToken()
+  next()
+})
+
 app.use('/', router)
 
+app.use(function(err, req, res, next) {
+  if (err) {
+    if (err.code == "EBADCSRFTOKEN") {
+      req.flash('errors', "Cross site request forgery detected.")
+      req.session.save(() => res.redirect('/'))
+    } else {
+      res.render("404")
+    }
+  }
+})
 
 const server = require('http').createServer(app)
-
 const io = require('socket.io')(server)
 
-io.use(function(socket, next){
+io.use(function(socket, next) {
   sessionOptions(socket.request, socket.request.res, next)
 })
 
-io.on('connection', function(socket){
-  if(socket.request.session.user){
+io.on('connection', function(socket) {
+  if (socket.request.session.user) {
     let user = socket.request.session.user
 
-      socket.emit('welcome',{username: user.username, avatar: user.avatar})
+    socket.emit('welcome', {username: user.username, avatar: user.avatar})
 
-    socket.on('chatMessageFromBrowser',function(data){
-      socket.broadcast.emit('chatMessageFromServer',{message:sanitizeHTML(data.message,{allowedTags:[],allowedAttributes:{}}), username:user.username, avatar:user.avatar})
+    socket.on('chatMessageFromBrowser', function(data) {
+      socket.broadcast.emit('chatMessageFromServer', {message: sanitizeHTML(data.message, {allowedTags: [], allowedAttributes: {}}), username: user.username, avatar: user.avatar})
     })
   }
 })
