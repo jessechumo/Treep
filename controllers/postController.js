325319c3c2508188e2d9ff231c4de26553e00a3a
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-09-11 20:27:28 +0300

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,6 @@
 const Post = require('../models/Post')
+const sendgrid = require('@sendgrid/mail')
+sendgrid.setApiKey(process.env.SENDGRIDAPIKEY )
 
 exports.viewCreateScreen = function(req, res) {
   res.render('create-post')
@@ -7,22 +9,39 @@ exports.viewCreateScreen = function(req, res) {
 exports.create = function(req, res) {
   let post = new Post(req.body, req.session.user._id)
   post.create().then(function(newId) {
-    req.flash("success","Your new post was successfully created")
-    req.session.save(()=>res.redirect(`/post/${newId}`))
+    sendgrid.send({
+      to:'youremail@gmail.com',
+      from: 'test@test.com',
+      subject: 'congrats on creating a new post',
+      text:'Your recent post is getting noticed',
+      html:'You did a <strong>great</strong> job.'
+    })
+    req.flash("success", "New post successfully created.")
+    req.session.save(() => res.redirect(`/post/${newId}`))
   }).catch(function(errors) {
-    errors.forEach(error => req.flash("errors",error))
-    req.session.save(()=>res.redirect("/create-post"))
+    errors.forEach(error => req.flash("errors", error))
+    req.session.save(() => res.redirect("/create-post"))
+  })
+}
+
+exports.apiCreate = function(req, res) {
+  let post = new Post(req.body, req.apiUser._id)
+  post.create().then(function(newId) {
+    res.json("Congrats.")
+  }).catch(function(errors) {
+    res.json(errors)
   })
 }
 
 exports.viewSingle = async function(req, res) {
   try {
-    let post = await Post.findSingleById(req.params.id,req.visitorId)
-    res.render('single-post-screen', {post: post , title:post.title})
+    let post = await Post.findSingleById(req.params.id, req.visitorId)
+    res.render('single-post-screen', {post: post, title: post.title})
   } catch {
     res.render('404')
   }
 }
+
 exports.viewEditScreen = async function(req, res) {
   try {
     let post = await Post.findSingleById(req.params.id, req.visitorId)
@@ -36,49 +55,58 @@ exports.viewEditScreen = async function(req, res) {
     res.render("404")
   }
 }
-exports.edit = function(req,res){
-  let post = new Post(req.body, req.visitorId,req.params.id)
-  post.update().then((status)=>{
-    //post was succesfully updated inthe database
-    //user does indeed have permission but there were validation errors
-    if(status=="success"){
- //post was updated in db
-    req.flash("success","Post succesfully updated. ")
-    req.session.save(function(){
-      res.redirect(`/post/${req.params.id}/edit`)
-    })
-    }else{
-     post.errors.forEach(function(error){
-       req.flash("errors",error)
-     })
-     req.session.save(function(){
-       res.redirect(`/post/${req.params.id}/edit`)
-     })
-    }
 
-  }).catch(()=>{
-    // if post with requested id  doesnt exist
-    //or if current user is not owner of the requested  post
-    req.flash("errors","you do not have permission to perform that action")
-    req.session.save(function(){
+exports.edit = function(req, res) {
+  let post = new Post(req.body, req.visitorId, req.params.id)
+  post.update().then((status) => {
+    // the post was successfully updated in the database
+    // or user did have permission, but there were validation errors
+    if (status == "success") {
+      // post was updated in db
+      req.flash("success", "Post successfully updated.")
+      req.session.save(function() {
+        res.redirect(`/post/${req.params.id}/edit`)
+      })
+    } else {
+      post.errors.forEach(function(error) {
+        req.flash("errors", error)
+      })
+      req.session.save(function() {
+        res.redirect(`/post/${req.params.id}/edit`)
+      })
+    }
+  }).catch(() => {
+    // a post with the requested id doesn't exist
+    // or if the current visitor is not the owner of the requested post
+    req.flash("errors", "You do not have permission to perform that action.")
+    req.session.save(function() {
       res.redirect("/")
     })
   })
 }
-exports.delete = function(req,res){
- Post.delete(req.params.id,req.visitorId).then(()=>{
-   req.flash("success","post successfully deleted.")
-   req.session.save(()=>res.redirect(`/profile/${req.session.user.username}`))
- }).catch(()=>{
-   req.flash("errors","You do not have permission to perform this action")
-   req.session.save(()=>res.redirect("/"))
- })
+
+exports.delete = function(req, res) {
+  Post.delete(req.params.id, req.visitorId).then(() => {
+    req.flash("success", "Post successfully deleted.")
+    req.session.save(() => res.redirect(`/profile/${req.session.user.username}`))
+  }).catch(() => {
+    req.flash("errors", "You do not have permission to perform that action.")
+    req.session.save(() => res.redirect("/"))
+  })
 }
 
-exports.search = function(req, res){
-  Post.search(req.body.searchTerm).then(posts=>{
+exports.apiDelete = function(req, res) {
+  Post.delete(req.params.id, req.apiUser._id).then(() => {
+    res.json("Success")
+  }).catch(() => {
+    res.json("You do not have permission to perform that action.")
+  })
+}
+
+exports.search = function(req, res) {
+  Post.search(req.body.searchTerm).then(posts => {
     res.json(posts)
-  }).catch(()=>{
+  }).catch(() => {
     res.json([])
   })
 }
