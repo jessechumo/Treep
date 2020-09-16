d718e0a2f79295c2fdd09431e521fb8931c56d6f
jesse kiprotich <40341693+Jesse-Bett@users.noreply.github.com>
2020-09-16 16:32:27 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const mongodb = require('mongodb')
-const connectionString =' mongodb+srv://todoAppUser:Kiprotich254@cluster0-eawpi.mongodb.net/Treep?retryWrites=true&w=majority'
+
 mongodb.connect(process.env.CONNECTIONSTRING,{useNewUrlParser:true,useUnifiedTopology:true},function(err,client) {
  module.exports = client   
  const app = require('./app')
